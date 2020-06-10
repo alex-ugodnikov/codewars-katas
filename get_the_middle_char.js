96a9d3aee98f34dd307a2f69f5486ec59c4226e0
Alexey Ugodnikov <alex.ugodnikov@gmail.com>
2020-06-10 16:40:15 -0400

    Adding kata description

--- get_the_middle_char.js
+++ get_the_middle_char.js
@@ -1,10 +1,6 @@
 // ************************************************************************************
 // https://www.codewars.com/kata/56747fd5cb988479af000028/train/javascript
-// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
-// It should remove all values from list a, which are present in list b.
-// array_diff([1,2],[1]) == [2]
-// If a value is present in b, all of its occurrences must be removed from the other:
-// array_diff([1,2,2,2,3],[2]) == [1,3]
+// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
 // ************************************************************************************
 
 
